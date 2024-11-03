4cecc669ac8899aa382794a91c5d15331a7d494f
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2024-11-04 00:52:50 +0530

    Update block.sh

--- block.sh
+++ block.sh
@@ -109,8 +109,8 @@ After=network.target
 
 [Service]
 User=$USER
-ExecStart=/root/blockmesh/target/release/blockmesh-cli login --email '$email' --password '$password'
-WorkingDirectory=/root/blockmesh/target/release
+ExecStart=/root/blockmesh/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email '$email' --password '$password' 
+WorkingDirectory=/root/blockmesh/target/x86_64-unknown-linux-gnu/release
 Restart=on-failure
 
 [Install]
